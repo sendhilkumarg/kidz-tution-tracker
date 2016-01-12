d1019eca4fdb505647440b9af8f947bd23e8460c
sendhilkumarg <sendhilkumarg@gmail.com>
2016-01-13 08:36:17 +1100

    Initial Commit

---
+++ kidz tution tracker/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  kidz tution tracker
+//
+//  Created by Sendhil kumar Gurunathan on 1/13/16.
+//  Copyright © 2016 Sendhil kumar Gurunathan. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
