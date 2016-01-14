74ceb909c495aa48263e2c8f24ae2ba653497844
sendhilkumarg <sendhilkumarg@gmail.com>
2016-01-14 23:24:15 +1100

    added individual story boards added segment control implementation for attendance

---
+++ kidz tution tracker/AttendanceViewController.swift
@@ -0,0 +1,95 @@
+//
+//  FirstViewController.swift
+//  kidz tution tracker
+//
+//  Created by Sendhil kumar Gurunathan on 1/13/16.
+//  Copyright © 2016 Sendhil kumar Gurunathan. All rights reserved.
+//
+
+import UIKit
+
+class AttendanceViewController: UIViewController {
+
+  
+    @IBOutlet weak var containerView: UIView!
+     weak var currentViewController: UIViewController?
+
+    
+    override func viewDidLoad() {
+        self.currentViewController = self.storyboard?.instantiateViewControllerWithIdentifier("pendingAttendance")
+        self.currentViewController!.view.translatesAutoresizingMaskIntoConstraints = false
+        self.addChildViewController(self.currentViewController!)
+        self.addSubview(self.currentViewController!.view, toView: self.containerView)
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func addSubview(subView:UIView, toView parentView:UIView) {
+        parentView.addSubview(subView)
+        
+        
+        var viewBindingsDict = [String: AnyObject]()
+        viewBindingsDict["subView"] = subView
+        parentView.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|[subView]|",
+            options: [], metrics: nil, views: viewBindingsDict))
+        parentView.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|[subView]|",
+            options: [], metrics: nil, views: viewBindingsDict))
+
+    }
+/*
+    func cycleFromViewController(oldViewController: UIViewController, toViewController newViewController: UIViewController) {
+        oldViewController.willMoveToParentViewController(nil)
+        self.addChildViewController(newViewController)
+        self.addSubview(newViewController.view, toView:self.containerView!)
+        newViewController.view.alpha = 0
+        newViewController.view.layoutIfNeeded()
+        UIView.animateWithDuration(0.5, animations: {
+            newViewController.view.alpha = 1
+            oldViewController.view.alpha = 0
+            },
+            completion: { finished in
+                oldViewController.view.removeFromSuperview()
+                oldViewController.removeFromParentViewController()
+                newViewController.didMoveToParentViewController(self)
+        })
+    }
+    */
+    func cycleFromViewController(oldViewController: UIViewController, toViewController newViewController: UIViewController) {
+        oldViewController.willMoveToParentViewController(nil)
+        self.addChildViewController(newViewController)
+        self.addSubview(newViewController.view, toView:self.containerView!)
+        // TODO: Set the starting state of your constraints here
+        newViewController.view.layoutIfNeeded()
+        
+        // TODO: Set the ending state of your constraints here
+        
+        UIView.animateWithDuration(0.5, animations: {
+            // only need to call layoutIfNeeded here
+            newViewController.view.layoutIfNeeded()
+            },
+            completion: { finished in
+                oldViewController.view.removeFromSuperview()
+                oldViewController.removeFromParentViewController()
+                newViewController.didMoveToParentViewController(self)
+        })
+    }
+    
+  
+    @IBAction func showComponent(sender: UISegmentedControl) {
+        
+        if sender.selectedSegmentIndex == 0 {
+            let newViewController = self.storyboard?.instantiateViewControllerWithIdentifier("pendingAttendance")
+            newViewController!.view.translatesAutoresizingMaskIntoConstraints = false
+            self.cycleFromViewController(self.currentViewController!, toViewController: newViewController!)
+            self.currentViewController = newViewController
+        } else {
+            let newViewController = self.storyboard?.instantiateViewControllerWithIdentifier("historyAttendance")
+            newViewController!.view.translatesAutoresizingMaskIntoConstraints = false
+            self.cycleFromViewController(self.currentViewController!, toViewController: newViewController!)
+            self.currentViewController = newViewController
+        }
+    }
+
+}
+
